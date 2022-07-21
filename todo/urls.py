581e703cba5caa85440a83ec283d8003a4d58ce8
jhff32 <66910676+jhff32@users.noreply.github.com>
2022-07-21 23:44:19 +0300

    login user

--- todo/urls.py
+++ todo/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from todoapp import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Auth
     path('signup/', views.signup_user, name='signup_user'),
     path('logout/', views.logout_user, name='logout_user'),
+    path('login/', views.login_user, name='login_user'),
 
     # Todos
     path('current/', views.current_todos, name='current_todos'),
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
@@ -31,6 +31,19 @@ def signup_user(request):
 
 def current_todos(request):
     return render(request, 'todoapp/current_todos.html')
+
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'todoapp/login_user.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todoapp/login_user.html',
+                          {'form': AuthenticationForm(), 'error': 'user or password did not match'})
+        else:
+            login(request, user)
+            return redirect('current_todos')
 
 
 def logout_user(request):
